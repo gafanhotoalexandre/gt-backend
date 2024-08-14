38129f8b4d81d77fbb83b23dbb6d92824a20eafc
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-08-14 13:53:40 -0300

    model type para Product

---
+++ src/models/Product.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod'
+
+export const productSchema = z.object({
+  enabled: z.boolean().default(false),
+  name: z.string().min(1, 'O nome do produto é obrigatório.'),
+  slug: z.string().min(1, 'O slug é obrigatório.'),
+  useInMenu: z.boolean().default(false),
+  stock: z.number().min(0, 'O estoque deve ser maior ou igual a 0.'),
+  description: z.string().optional(),
+  price: z.number().min(0, 'O preço deve ser maior ou igual a 0.'),
+  priceWithDiscount: z
+    .number()
+    .min(0, 'O preço com desconto deve ser maior ou igual a 0.'),
+  categoryIds: z.array(z.number().positive()).optional(),
+  images: z
+    .array(
+      z.object({
+        type: z.string().min(1, 'O tipo da imagem é obrigatório.'),
+        content: z.string().min(1, 'O conteúdo da imagem é obrigatório.'),
+      })
+    )
+    .optional(),
+  options: z
+    .array(
+      z.object({
+        title: z.string().min(1, 'O título da opção é obrigatório.'),
+        shape: z.enum(['SQUARE', 'CIRCLE']).default('SQUARE'),
+        radius: z.number().optional(),
+        type: z.enum(['TEXT', 'COLOR']).default('TEXT'),
+        values: z
+          .array(z.string())
+          .min(1, 'Os valores da opção são obrigatórios.'),
+      })
+    )
+    .optional(),
+})
+
+export type Product = z.infer<typeof productSchema>
